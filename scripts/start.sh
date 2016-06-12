ab2938f93142d1eb1c5e3fa6c4833b3a1a792a50
Leon Li <lilei.leon@gmail.com>
2016-06-12 13:17:11 +0800

    change start script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 chown -R ubuntu:ubuntu /opt/node_home
-su - ubuntu -c "export NODE_ENV=production pm2 start /opt/node_home/index.js --name=ghost"
+su - ubuntu -c "export NODE_ENV=production"
+su - ubuntu -c "pm2 start /opt/node_home/index.js --name=ghost"
